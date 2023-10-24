fa874c4d0178e2247146fe48b6aaa380bbcdb510
Wahyu Maulana Aditya <first.wahyumaul21@gmail.com>
2023-10-25 03:32:27 +0700

    Update

--- app/src/main/java/com/example/waystoryapp/view/story/detail/DetailStoryActivity.kt
+++ app/src/main/java/com/example/waystoryapp/view/story/detail/DetailStoryActivity.kt
@@ -1,12 +1,12 @@
-package com.example.waystoryapp.view.story
+package com.example.waystoryapp.view.story.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.waystoryapp.R
 
-class AddStoryActivity : AppCompatActivity() {
+class DetailStoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_story)
+        setContentView(R.layout.activity_detail_story)
     }
 }
