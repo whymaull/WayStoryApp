7f6aecf8608141a28a2276ed82795f38d97ae88d
Wahyu Maulana Aditya <first.wahyumaul21@gmail.com>
2023-11-01 00:06:34 +0700

    Update fun postStory

--- app/src/main/java/com/example/waystoryapp/data/api/ApiService.kt
+++ app/src/main/java/com/example/waystoryapp/data/api/ApiService.kt
@@ -11,6 +11,7 @@ import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
 import retrofit2.http.Path
@@ -32,10 +33,12 @@ interface ApiService {
         @Field("password") password: String,
     ): Call<ResponseLogin>
 
+    @Multipart
     @POST("stories")
     fun postStory(
+        @Header("Authorization")token : String,
         @Part file: MultipartBody.Part,
-        @Part("description") description: String,
+        @Part ("deskripsi")deskripsi: String
     ): Call<AddStoryResponse>
 
     @GET("stories")
