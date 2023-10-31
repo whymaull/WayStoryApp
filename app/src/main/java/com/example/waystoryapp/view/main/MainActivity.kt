880b628204cfa241f10ee55da98be64b55459d9d
Wahyu Maulana Aditya <first.wahyumaul21@gmail.com>
2023-11-01 05:40:12 +0700

    Update

--- app/src/main/java/com/example/waystoryapp/view/main/MainActivity.kt
+++ app/src/main/java/com/example/waystoryapp/view/main/MainActivity.kt
@@ -2,13 +2,10 @@ package com.example.waystoryapp.view.main
 
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.view.WindowInsets
-import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
